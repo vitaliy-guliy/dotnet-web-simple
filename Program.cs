258d1e5958a0c1196d8afaa344253d828593266a
Valeriy Svydenko <vsvydenk@redhat.com>
2022-11-22 15:51:41 +0200

    chore: CRW-2456 set TargetFramework = net7.0 (#7)
    * chore: update web sample to use net7.0
    
    Signed-off-by: Valerii Svydenko <vsvydenk@redhat.com>

--- Program.cs
+++ Program.cs
@@ -2,30 +2,9 @@
  *  Copyright (c) Red Hat, Inc. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-namespace web
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+app.MapGet("/", () => "Hello World!");
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://0.0.0.0:5000/")
-		.Build();
-	
-    }
-}
+app.Run();
--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 # dotnet-web-simple application
 
-It is a simple dotnet web application which uses `netcoreapp7.0` as a target framework.
+It is a simple dotnet web application which uses `net7.0` as a target framework.
 
  
 # Application Output
